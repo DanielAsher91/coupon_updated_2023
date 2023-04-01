2dfbb1a3731684d3a3f760752db284a8e76e8c04
DanielAsher91 <dkhalfon24@gmail.com>
2023-04-01 17:13:07 +0300

    commit to first depo

--- src/main/java/portofolio/couponSystemUpdated/CouponSystemUpdatedApplication.java
+++ src/main/java/portofolio/couponSystemUpdated/CouponSystemUpdatedApplication.java
@@ -2,12 +2,15 @@ package portofolio.couponSystemUpdated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CouponSystemUpdatedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouponSystemUpdatedApplication.class, args);
+
+		ConfigurableApplicationContext ctx =SpringApplication.run(CouponSystemUpdatedApplication.class, args);
+
 	}
 
 }
